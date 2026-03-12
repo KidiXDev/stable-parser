767f82c32b485a1df5efa56bb8ca25be2dd6dba0
KidiXDev <dikhiardian55@gmail.com>
2026-03-12 13:36:35 +0700

    cleanup

--- src/main/ipcHandlers/utils/parser.ts
+++ src/main/ipcHandlers/utils/parser.ts
@@ -1,4 +1,3 @@
-// Re-export shared types and parsers for backward compatibility
 export type { ExifTag, ImageParameters as A1111Parameters } from './types'
 export { extractA1111Parameters } from './a1111-parser'
 export { extractComfyUIParameters, isComfyUIWorkflow } from './comfyui-parser'
